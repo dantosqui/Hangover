73cc2419eaeff9aead3adc2dff472e4f3ef2a379
eitan <mail@mail.com>
2024-05-31 12:28:31 -0300

    Autoincremental en dbconfig.js

--- repositories/dbconfig.js
+++ repositories/dbconfig.js
@@ -8,3 +8,12 @@ export const DBConfig = {
     host: process.env.DB_HOST
 };
 
+/*
+
+Poner esto en la base de datos para que las provincias sean autoincremental a partir de 95
+
+SELECT pg_get_serial_sequence('public.provinces', 'id');
+
+ALTER SEQUENCE public.provinces_id_seq RESTART WITH 95;
+
+*/
