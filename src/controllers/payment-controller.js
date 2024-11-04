efeb43486091de7c07463cfd5cbd7ad84c3a04a3
DANTE VERDI GUTIERREZ <47573312@ort>
2024-11-04 11:02:11 -0300

    616 southside

--- src/controllers/payment-controller.js
+++ src/controllers/payment-controller.js
@@ -1,14 +1,39 @@
 import express from "express";
-import {
-  createOrder,
-  receiveWebhook,
-} from "../utils/paymentUtils.js";
+
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+const client = new MercadoPagoConfig({ accessToken: 'YOUR_ACCESS_TOKEN' });
 
 const router = express.Router();
 
-router.post("/create-order", createOrder);
+router.post("/create-oreference", async (req, res) => {
+  try{
+    const body = {
+      items: [{
+        title: req.body.title,
+        total_price: Number(req.body.total_price),
+        currency_id: "ARS",
+      }],
+      back_urls: {
+        success: "",
+        failure:"",
+        pending: ""
+      },
+      auto_return: "approved",
+    };
 
-router.post("/webhook", receiveWebhook);
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
+    res.json({
+      id: result.id,
+    });
+  }catch (error){
+    console.log(error);
+    res.status(500).json({
+      error: "Error al crear la preferencia :(",
+    });
+  }
+});
+
 
 router.get("/success", (req, res) => res.send("Success"));
 
